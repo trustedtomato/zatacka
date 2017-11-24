bdf7b4685ddd377876733bd77a524d278d34862b
Halasi Tamás <trusted.tomato@gmail.com>
2017-11-24 22:23:38 +0100

    Use circle instead of square & other thingies

--- main.js
+++ main.js
@@ -2,10 +2,23 @@
 
 const {cos, sin, sqrt, random, floor, round, PI} = Math;
 
+const includes = (elem, container) =>
+	typeof elem !== 'object'
+	? typeof elem === 'undefined' || container === elem
+	: Object.keys(elem).every(key => includes(container[key], elem[key]));
 const radiansToVec = rad => ({x: cos(rad), y: sin(rad)});
 const multiplyVec = ({x,y}, n) => ({x: x*n, y: y*n});
 const log = x => { console.log(x); return x; };
 const waitFrame = () => new Promise(requestAnimationFrame);
+const waitEvent = (el, eName, props) => new Promise(resolve => {
+	const listener = e => {
+		if(!includes(props, e)) return;
+		resolve(e);
+		document.removeEventListener(eName, listener);
+	};
+	document.addEventListener(eName, listener);
+});
+const waitKeydown = props => waitEvent(document, 'keydown', props);
 
 
 const canvas = document.querySelector('canvas');
@@ -16,21 +29,31 @@ const ctx = canvas.getContext('2d', {alpha: false});
 const startParty = async (snakeTypes, length) => {
 	const width = Math.floor(window.innerWidth);
 	const height = Math.floor(window.innerHeight);
+	canvas.width = width;
+	canvas.height = height;
+
 	const hwRatio = height / width;
 	const speed = 1;
 	const rotationSpeed = PI / 140;
 	const inheriting = false;
-	canvas.width = width;
-	canvas.height = height;
+	const holeFrom = 86;
+	const holeCycle = 100;
 	
-	const colors = new Set(snakeTypes.map(snakeType => snakeType.color));
+	const colors = new Set(snakeTypes.map(snakeType => snakeType.color).concat(['255,255,255']));
 	const scores = new WeakMap(snakeTypes.map(snakeType => [snakeType, 0]));
 	ctx.clearRect(0, 0, width, height);
 	for(let i = 0; i < length; i++){
+		if(!inheriting){
+			ctx.clearRect(0, 0, width, height);
+		}
+
 		const states = new WeakMap(snakeTypes.map(snakeType => [snakeType, {
 			dir: PI * 2 * random() - PI,
 			left: false,
 			right: false,
+			holeI: 0,
+			// TODO: use holes
+			holeStart: {x: NaN, y: NaN}
 			// TODO: Distrubute the snakes evenly
 			x: random() * width,
 			y: random() * height
@@ -65,6 +88,18 @@ const startParty = async (snakeTypes, length) => {
 				const snakeType = living[i];
 
 				const state = states.get(snakeType);
+				state.holeI++;
+				if(state.holeI >= holeCycle){
+					state.holeI = 0;
+				}
+				if(state.holeI >= holeFrom){
+					ctx.translate(state.x, state.y);
+					ctx.rotate(state.dir);
+					ctx.clearRect(-2.5, -2.5, 5, 5);
+					ctx.rotate(-state.dir);
+					ctx.translate(-state.x, -state.y);
+				}
+				
 				if(state.left){
 					state.dir -= rotationSpeed;
 					if(state.dir <= -PI){
@@ -83,7 +118,10 @@ const startParty = async (snakeTypes, length) => {
 				state.y = state.y + moveVector.y;
 
 				ctx.fillStyle = 'rgb(' + snakeType.color + ')';
-				ctx.fillRect(state.x - 2, state.y - 2, 4, 4);
+				ctx.beginPath();
+				ctx.arc(state.x, state.y, 2, 0, PI*2);
+				ctx.fill();
+				ctx.closePath();
 
 				const corners = [];
 				// top left
@@ -114,20 +152,18 @@ const startParty = async (snakeTypes, length) => {
 				}
 			}
 
-			if(living.length <= 1) break gameLoop;
+			if(living.length <= 0) break gameLoop;
 			await waitFrame();
 		}
 
 		document.removeEventListener('keydown', handleKeydown);
 		document.removeEventListener('keyup', handleKeyup);
 
-		if(!inheriting){
-			ctx.clearRect(0, 0, width, height);
-		}
+		await waitKeydown({code: 'Space'});
 	}
 };
 
 startParty([
-	{left: 'Backquote', right: 'Digit1', color: '255,0,0'},
+	// {left: 'Backquote', right: 'Digit1', color: '255,0,0'},
 	{left: 'ArrowDown', right: 'ArrowRight', color: '0,255,0'}
 ], 10);
